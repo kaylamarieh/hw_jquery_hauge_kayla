02ce866717de5aa6fbae33d75835e7815ec79d6e
Kayla Hauge <kmhauge@ufl.edu>
2018-04-08 15:31:59 -0400

    change styling on paragraph elements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
     $('img').remove();
+
+    var paragraphs = $('p');
+
+    $(paragraphs).css({'padding': '5%', 'background-color': '#e5e5f2', 'color': '#000080' });
 });
